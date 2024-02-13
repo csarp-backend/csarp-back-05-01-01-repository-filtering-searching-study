2f151a65e13b497651993bc27a88751f89879f1a
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-13 09:46:34 +0100

    A repository interface kiegészül  a szűrést és keresést megvalósító metódussal

--- Kreata.Backend/Repos/IStudentRepo.cs
+++ Kreata.Backend/Repos/IStudentRepo.cs
@@ -1,8 +1,10 @@
 ﻿using Kreta.Shared.Models;
+using Kreta.Shared.Parameters;
 
 namespace Kreta.Backend.Repos
 {
     public interface IStudentRepo : IRepositoryBase<Student>
     {
+        public IQueryable<Student> GetStudents(StudentQueryParameters parameters);
     }
 }
